b9895f52c2ba1e72169fa0bff6ce9316e50fa4ab
maksimovaflagsoft <maksimova@flagsoft.ru>
2025-04-29 03:59:07 +0500

    firstTest

--- tests/e2e/firstTests.spec.ts
+++ tests/e2e/firstTests.spec.ts
@@ -6,15 +6,29 @@ test.describe.parallel('Базовая проверка сайта', () => {
 
         test('Проверка 111', async ({page})=> {
                 await page.goto('https://magento.softwaretestingboard.com/')
+                const whatsNew = await page.locator('#ui-id-3')
+                await expect(whatsNew).toContainText('W')
+
             })
+            
+        test('Проверка 112', async ({page})=> {
+            await page.goto('https://magento.softwaretestingboard.com/')
+            await page.hover('#ui-id-4')
+            const ui = await page.locator('.level1')
+            await expect(ui).toHaveCount(2)
+
+        })
+            
     })
 
-    test.describe.parallel('Базовая проверка сайта 2', () => {
+    // test.describe.parallel('Базовая проверка сайта 2', () => {
 
-        test('Проверка 123', async ({page})=> {
-                await page.goto('https://example.com/')
-            })
-    })
+    //     test('Проверка 123', async ({page})=> {
+    //             await page.goto('https://example.com/')
+    //         })
+    // })
 
 
 })
+function privet(parameters){body}
+(parameters)=>{body}
